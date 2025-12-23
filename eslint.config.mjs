76177c2b4c19031893828325eb9fb53787ff570b
mrap10 <abhi2anu04@gmail.com>
2025-12-23 17:17:11 +0530

    feat: update configs with prettier, eslint rules && initial project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  eslintConfigPrettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
